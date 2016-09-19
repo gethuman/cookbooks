f2fe1728fc8cdf01dc4a9bb8ec22a5f20c156e8f
Will Button <will@willbutton.co>
2016-09-19 09:24:24 -0700

    modifying logs template to try and get hostname

--- logs/recipes/config.rb
+++ logs/recipes/config.rb
@@ -1,4 +1,8 @@
 template "/tmp/cwlogs.cfg" do
+  instance = search("aws_opsworks_instance").first
+  variables(
+    :host => "#{instance['hostname']}"
+  )
   cookbook "logs"
   source "cwlogs.cfg.erb"
   owner "root"
