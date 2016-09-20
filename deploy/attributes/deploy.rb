973df9f8b099dbead207c3cbbe08ad1b0a58e934
Will Button <will@willbutton.co>
2016-09-20 09:16:43 -0700

    Removing more rails code

--- deploy/attributes/deploy.rb
+++ deploy/attributes/deploy.rb
@@ -42,8 +42,6 @@ default[:opsworks][:deploy_chef_provider] = 'Timestamped'
 case node[:platform]
 when 'debian','ubuntu'
   default[:opsworks][:deploy_user][:group] = 'www-data'
-when 'centos','redhat','fedora','amazon'
-  default[:opsworks][:deploy_user][:group] = node['opsworks']['rails_stack']['name'] == 'nginx_unicorn' ? 'nginx' : 'apache'
 end
 
 default[:opsworks][:rails][:ignore_bundler_groups] = ['test', 'development']
