7bb813726e1d22c04cbae1ebfcb311d351f70e20
Will Button <will@willbutton.co>
2016-10-06 16:55:01 -0700

    separating setup and deploy concerns

--- appserver/recipes/default.rb
+++ appserver/recipes/default.rb
@@ -13,6 +13,7 @@ end
 instance = search("aws_opsworks_instance", "self:true").first # this gets the databag for the instance
 layers = instance['role'] # the attribute formerly known as 'layers' via opsworks is now found as role in the opsworks instance
 
+# setting environment vars for shell access
 if layers.include?("api-layer")
     Chef::Log.info("** setting container to api")
     execute 'add api env var' do
@@ -28,4 +29,118 @@ else
     execute 'add unknown env var' do
       command 'echo CONTAINER="unknown" >> /root/.bashrc && export CONTAINER="unknown"'
     end
+end
+
+# building environment vars
+app = search("aws_opsworks_app").first
+app['environment'].each do |key,value|
+  env_var = env_var << "\"#{key}\":\"#{value}\","
+end
+
+if layers.include?("api-layer")
+    env_var = env_var + '"CONTAINER":"api"'
+elsif layers.include?("web-layer")
+    env_var = env_var + '"CONTAINER":"web"'
+else
+    env_var = env_var + '"CONTAINER":"unknown"'
+end
+
+directory '/etc/pm2/conf.d' do
+  owner 'root'
+  group 'root'
+  mode '0755'
+  recursive true
+  action :create
+  notifies :create, 'template[/etc/pm2/conf.d/server.json]', :immediately
+end
+
+template '/etc/pm2/conf.d/server.json' do
+  source 'server.erb'
+  owner 'root'
+  group 'root'
+  mode '0644'
+  variables :environments => { 'vars' => env_var }
+  action :nothing
+  notifies :create, 'file[/root/.ssh/id_rsa]', :immediately
+  notifies :create, 'directory[/tmp/.ssh]', :immediately
+end
+
+directory '/root/.ssh' do
+  owner 'root'
+  group 'root'
+  recursive true
+  action :create
+end
+
+file '/root/.ssh/id_rsa' do
+  content app['app_source']['ssh_key']
+  owner 'root'
+  group 'root'
+  mode '0600'
+  action :create_if_missing
+  notifies :touch, 'file[/root/.ssh/known_hosts]', :immediately
+end
+
+file '/root/.ssh/known_hosts' do
+  owner 'root'
+  group 'root'
+  action :nothing
+  notifies :run, 'execute[genssh]', :immediately
+end
+
+execute 'genssh' do
+  command "ssh-keygen -R bitbucket.org"
+  action :nothing
+  notifies :run, 'execute[add_known_hosts]', :immediately
+end
+
+execute 'add_known_hosts' do
+  command "ssh-keyscan -H bitbucket.org >> /root/.ssh/known_hosts"
+  action :nothing
+  notifies :create, 'directory[/tmp/.ssh]', :immediately
+end
+
+directory '/tmp/.ssh' do
+  owner 'root'
+  group 'root'
+  mode '0770'
+  recursive true
+  action :nothing
+  notifies :create, 'template[/tmp/.ssh/chef_ssh_deploy_wrapper.sh]', :immediately
+  notifies :create, 'file[/root/.ssh/id_rsa]', :immediately
+end
+
+template "/tmp/.ssh/chef_ssh_deploy_wrapper.sh" do
+  source "chef_ssh_deploy_wrapper.sh.erb"
+  owner 'root'
+  mode 0770
+  action :nothing
+  notifies :create, 'directory[/srv/www/app/current]', :immediately
+end
+
+
+directory '/srv/www/app/current' do
+  owner 'root'
+  group 'root'
+  mode '0644'
+  recursive true
+  action :nothing
+  notifies :create, 'directory[/srv/www/app/log]', :immediately
+end
+
+directory '/srv/www/app/log' do
+  owner 'root'
+  group 'root'
+  mode '0644'
+  recursive true
+  action :nothing
+  notifies :create, 'directory[/srv/www/app/releases]', :immediately
+end
+
+directory '/srv/www/app/releases' do
+  owner 'root'
+  group 'root'
+  mode '0644'
+  recursive true
+  action :nothing
 end
