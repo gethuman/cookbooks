1b30812561c3d3c6ef7ac5ca041a0d93847bd3ea
neeschit <nischit14@gmail.com>
2017-10-05 22:46:46 -0400

    add back in log rotate

--- appserver/recipes/default.rb
+++ appserver/recipes/default.rb
@@ -148,3 +148,16 @@ cron "cloudwatch_schedule_metrics" do
   user "root"
   command "/root/aws-scripts-mon/mon-put-instance-data.pl --mem-util --disk-space-util --disk-path=/ --from-cron"
 end
+
+file '/etc/cron.hourly/logrotate' do
+  mode 0755
+  owner 'root'
+  group 'root'
+  content '#!/bin/sh
+   /usr/sbin/logrotate /etc/logrotate.conf >/dev/null 2>&1
+   EXITVALUE=$?
+   if [ $EXITVALUE != 0 ]; then
+       /usr/bin/logger -t logrotate "ALERT exited abnormally with [$EXITVALUE]"
+   fi
+   exit 0'
+ end
